eefc25200cf4b7db5ce2880842d06945a7e78305
SelenaMilutin <selena.milutin@gmail.com>
2022-04-27 18:19:39 +0200

    Add model for MedicalRecord

--- models/entity/Appointment.cs
+++ models/entity/Appointment.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCareInfromationSystem.models.users;
 
 namespace HealthCareInfromationSystem.models.entity
 {
-	class Appointment
+	public enum AppointmentType
 	{
+		physical,
+		operation
+	}
+	public class Appointment
+	{
+		int id;
+		Person doctor;
+		Person patient;
+		Premise premise;
+		DateTime beginning;
+		int duration;
+		AppointmentType operation;
+		string comment;
+
+		public Appointment()
+		{
+		}
+
+		public Appointment(int id, Person doctor, Person patient, Premise premise,
+			DateTime beginning, int duration, AppointmentType operation, string comment)
+		{
+			this.id = id;
+			this.doctor = doctor;
+			this.patient = patient;
+			this.premise = premise;
+			this.beginning = beginning;
+			this.duration = duration;
+			this.operation = operation;
+			this.comment = comment;
+		}
 	}
 }
--- models/users/Doctor.cs
+++ models/users/Doctor.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace HealthCareInfromationSystem.models.users
 {
-	class Doctor
+	class Doctor : Person
 	{
+		string specialisation;
+
+
+		public Doctor() : base() { }
+		public Doctor(int id, string name, string lastName, Role role,
+					  string password, bool blocked, int blocker, string username) : base(id, name, lastName, role,
+						  password, blocked, blocker, username)
+		{ }
 	}
 }
