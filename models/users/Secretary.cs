b2ca8f2bece99f734718e585ea2b863205859b12
SelenaMilutin <96072558+SelenaMilutin@users.noreply.github.com>
2022-04-27 21:52:40 +0200

    Fix namespace

--- models/users/Secretary.cs
+++ models/users/Secretary.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HealthCareInfromationSystem
+namespace HealthCareInfromationSystem.models.users
 {
     class Secretary : Person
     {
