8cc00728ca94fe73a2fcfa3d968ae79f4658ff68
SelenaMilutin <selena.milutin@gmail.com>
2022-04-27 18:29:01 +0200

    Fix import for MedicalRecord

--- models/users/Patient.cs
+++ models/users/Patient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCareInfromationSystem.models.entity;
 
 namespace HealthCareInfromationSystem.models.users
 {
