ced333a2907d21cdebb48af3d2f0366bae28848d
SelenaMilutin <selena.milutin@gmail.com>
2022-04-27 18:15:20 +0200

    Add model for Patient

---
+++ models/users/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareInfromationSystem.models.users
+{
+	class Patient : Person
+	{
+		MedicalRecord medicalRecord;
+
+		public Patient() : base() { }
+		public Patient(int id, string name, string lastName, Role role,
+					  string password, bool blocked, int blocker, string username) : base(id, name, lastName, role,
+						  password, blocked, blocker, username)
+		{ }
+	}
+}
