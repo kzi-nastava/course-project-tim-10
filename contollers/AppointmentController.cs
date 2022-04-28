e6514e24674ab9daaff5bfd83d8c0b92d18fdac0
SelenaMilutin <selena.milutin@gmail.com>
2022-04-28 17:08:52 +0200

    Add function IsAvailable for checking valid user entry

--- contollers/AppointmentController.cs
+++ contollers/AppointmentController.cs
@@ -44,5 +44,50 @@ namespace HealthCareInfromationSystem.contollers
                 return appointments;
             }
         }
+
+        /*
+	    * Retrieves data from dataset
+
+            Parameters:
+                    connectionString(string): name of the connection
+                    queryString(string): query for retrieving data
+                    beginning(string): time that we want to start our appointment
+                    durration(string): how long it would last
+
+            Returns:
+                    bool value, true if we can book an appointment, false if we can not
+	    * */
+        public static bool IsAvailable(string connectionString, string queryString, string beginning, string duration)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+
+                OleDbCommand command = new OleDbCommand(queryString, connection);
+
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+
+                DateTime beginningNew = DateTime.ParseExact(beginning, "dd.MM.yyyy. HH:mm", null);
+                DateTime endingNew = beginningNew.AddMinutes(int.Parse(duration));
+                int c = 0;
+
+                while (reader.Read())
+                {
+                    c++;
+                    DateTime beginningOld = DateTime.ParseExact(reader[4].ToString(), "dd.MM.yyyy. HH:mm", null);
+                    DateTime endingOld = beginningOld.AddMinutes(int.Parse(reader[5].ToString()));
+                    if ((beginningOld <= beginningNew && beginningNew <= endingOld)
+                        || (beginningOld <= endingNew && endingNew <= endingOld))
+                    {
+                        Console.WriteLine(c);
+                        return false;
+                    }
+
+                }
+                Console.WriteLine(c);
+                reader.Close();
+                return true;
+            }
+        }
     }
 }
