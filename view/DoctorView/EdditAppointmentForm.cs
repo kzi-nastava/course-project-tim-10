ccab7bca51c2f99cab28b4df3f73b8e2603d99f0
SelenaMilutin <selena.milutin@gmail.com>
2022-04-28 21:18:07 +0200

    Add action on Save button on EdditAppointment

--- view/DoctorView/EdditAppointmentForm.cs
+++ view/DoctorView/EdditAppointmentForm.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using HealthCareInfromationSystem.models.entity;
 using HealthCareInfromationSystem.models.users;
 using HealthCareInfromationSystem.contollers;
+using HealthCareInfromationSystem.utils;
 
 namespace HealthCareInfromationSystem.view.DoctorView
 {
@@ -54,13 +55,66 @@ namespace HealthCareInfromationSystem.view.DoctorView
 			durationTextBox.Text = appointment.Duration.ToString();
 		}
 
-		private void SaveBtn_Click(object sender, EventArgs e)
+		private void SaveBtnClick(object sender, EventArgs e)
 		{
+			string premiseId = premiseComboBox.SelectedValue.ToString();
+			//Console.WriteLine(premiseComboBox.SelectedValue.ToString());
+			string patientId = patientComboBox.SelectedValue.ToString();
+			string beginning = beginningTextBox.Text;
+			string duration = durationTextBox.Text;
+			string type = typeComboBox.SelectedItem.ToString();
+			//Console.WriteLine(typeComboBox.SelectedItem.ToString());
+
+			//checking if data is correct
+			try
+			{
+				int durationCheck = int.Parse(duration);
+				DateTime beginningCheck = DateTime.ParseExact(beginning, "dd.MM.yyyy. HH:mm", null);
+			}
+			catch
+			{
+				MessageBox.Show("Please check beginning or duration fild and enter correct values.", "Error");
+				return;
+			}
+
+			//checking if the doctor is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+				"select * from appointments where doctorId=\"" + LoggedInUser.loggedIn.Id + "\" and not id=\"" + appointmentId + "\"",
+				beginning, duration))
+			{
+				MessageBox.Show("Doctor has an appointment.", "Error");
+				return;
+			}
+
+			//checking if the room is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+			"select * from appointments where premiseId=\"" + premiseId + "\" and not id=\"" + appointmentId + "\"",
+			beginning, duration))
+			{
+				MessageBox.Show("Premise occupied.", "Error");
+				return;
+			}
+
+			//checking if the patient is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+			"select * from appointments where patientId=\"" + patientId + "\" and not id=\"" + appointmentId + "\"",
+			beginning, duration))
+			{
+				MessageBox.Show("Patient has an appointment.", "Error");
+				return;
+			}
+
+			DialogResult dialogResult = MessageBox.Show("Are you sure you want to save changes?", "Check", MessageBoxButtons.YesNo);
+			if (dialogResult == DialogResult.Yes)
+			{
+				MessageBox.Show("Changes saved.", "Success");
+				//TO DO CHANGE BASE
+			}
 			
 
 		}
 
-		private void CancelBtn_Click(object sender, EventArgs e)
+		private void CancelBtnClick(object sender, EventArgs e)
 		{
 			this.Close();
 		}
