fc21bd4be28cbe12a872ac4c0c33afa87d0a6db8
SelenaMilutin <selena.milutin@gmail.com>
2022-04-28 16:59:36 +0200

    Add function for loading information for combobox

--- contollers/PremiseController.cs
+++ contollers/PremiseController.cs
@@ -42,5 +42,36 @@ namespace HealthCareInfromationSystem.contollers
                 return premise;
             }
         }
+
+        /*
+	    * Creates dictionary used for filling in a combobox
+
+            Parameters:
+                    connectionString(string): name of the connection
+                    queryString(string): query for retrieving data
+
+            Returns:
+                    Dictionary where key = premise id(firs value in query) and value = premise name(second value in query).
+	    * */
+        public static Dictionary<string, string> RetrievePair(string connectionString, string queryString)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+
+                OleDbCommand command = new OleDbCommand(queryString, connection);
+
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+                Dictionary<string, string> premise = new Dictionary<string, string>();
+
+                while (reader.Read())
+                {
+                    premise.Add(reader[0].ToString(), reader[1].ToString());
+
+                }
+                reader.Close();
+                return premise;
+            }
+        }
     }
 }
