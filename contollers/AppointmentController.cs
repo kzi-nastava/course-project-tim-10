cf4a8eaf61892b2effaf4bb752c4c6600484d3e8
SelenaMilutin <selena.milutin@gmail.com>
2022-04-28 21:07:37 +0200

    Add function for loading one appointment

--- contollers/AppointmentController.cs
+++ contollers/AppointmentController.cs
@@ -89,5 +89,25 @@ namespace HealthCareInfromationSystem.contollers
                 return true;
             }
         }
+
+        public static Appointment LoadOneAppointment(string connectionString, string queryString)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+
+                OleDbCommand command = new OleDbCommand(queryString, connection);
+
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+                Appointment appointment = null;
+
+                while (reader.Read())
+                {
+                    appointment = Appointment.Parse(reader);
+                }
+                reader.Close();
+                return appointment;
+            }
+        }
     }
 }
