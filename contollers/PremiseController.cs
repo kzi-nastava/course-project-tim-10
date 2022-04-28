c335ecc8b1d3cf1a42a7ffaa4929b9ca1f3d6b7f
miloss01 <mikicamiki.bat@gmail.com>
2022-04-28 21:21:16 +0200

    Add premise controller

---
+++ contollers/PremiseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareInfromationSystem.contollers
+{
+    class PremiseController
+    {
+        public bool CheckIfPremiseExistsById(String id)
+        {
+            using (OleDbConnection connection = new OleDbConnection(Constants.connectionString))
+            {
+                connection.Open();
+                String query = $"select * from premises where premises_id = \"{id}\"";
+                OleDbCommand command = new OleDbCommand(query, connection);
+                OleDbDataReader reader = command.ExecuteReader();
+                return reader.HasRows;
+            }
+        }
+    }
+}
--- view/ManagerView/PremisesCRUDForm.cs
+++ view/ManagerView/PremisesCRUDForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthCareInfromationSystem.contollers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace HealthCareInfromationSystem.view.ManagerView
 {
     public partial class PremisesCRUDForm : Form
     {
+        private PremiseController premiseController = new PremiseController();
+
         public PremisesCRUDForm()
         {
             InitializeComponent();
+        }
+
+        private void SetLabelsAndButtons()
+        {
+            label1.Text = "Premise ID";
+            label2.Text = "Name";
+            label3.Text = "Premise type";
+            label4.Text = "";
+            button1.Text = "Add new";
+            button2.Text = "Edit";
+            button3.Text = "Delete";
+            button4.Text = "Clear";
+        }
+
+        private void FillComboBox()
+        {
+            comboBox1.Items.Add("operational room");
+            comboBox1.Items.Add("examination room");
+            comboBox1.Items.Add("rest room");
+            comboBox1.Items.Add("other");
         }
     }
 }
