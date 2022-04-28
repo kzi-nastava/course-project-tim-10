744e40b13450e47c84788dd5b3e219c266c0a900
miloss01 <mikicamiki.bat@gmail.com>
2022-04-28 21:26:27 +0200

    Add functions for filling table in premises crud form

--- view/ManagerView/PremisesCRUDForm.cs
+++ view/ManagerView/PremisesCRUDForm.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,6 +40,30 @@ namespace HealthCareInfromationSystem.view.ManagerView
             comboBox1.Items.Add("examination room");
             comboBox1.Items.Add("rest room");
             comboBox1.Items.Add("other");
+        }
+
+        private void FillTable(String query, OleDbConnection connection)
+        {
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            //OleDbCommandBuilder commandBuilder = new OleDbCommandBuilder(adapter);
+            //BindingSource bindingSource = new BindingSource();
+            //dataGridView1.DataSource = bindingSource;
+            DataTable table = new DataTable
+            {
+                Locale = CultureInfo.InvariantCulture
+            };
+
+            adapter.Fill(table);
+            //bindingSource.DataSource = table;
+            dataGridView1.DataSource = table;
+        }
+
+        private void RefreshTable()
+        {
+            using (OleDbConnection connection = new OleDbConnection(Constants.connectionString))
+            {
+                FillTable("select * from premises where type <> \"warehouse\"", connection);
+            }
         }
     }
 }
