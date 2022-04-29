db4a11521fe8518b3a298d5249a9d9dda07148e9
SelenaMilutin <selena.milutin@gmail.com>
2022-04-29 04:18:42 +0200

    Add MedicalRecordController and function for loading one medical record

--- view/DoctorView/AppointmentsByDateForm.cs
+++ view/DoctorView/AppointmentsByDateForm.cs
@@ -47,7 +47,21 @@ namespace HealthCareInfromationSystem.view.DoctorView
 
 		private void MoreInfoBtnClick(object sender, EventArgs e)
 		{
-			
+			int selectedRowCount =
+			dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+			if (selectedRowCount == 1)
+			{
+				string patientId = AppointmentController.GetPatientId(Constants.connectionString,
+				"select patientId from appointments where id=\"" + GetSelectedAppointmentId() + "\"");
+				MedicalRecord medical = MedicalRecordController.LoadMedical(Constants.connectionString,
+				"select * from medical_record where patientId=\"" + patientId + "\"");
+				//MedicalRecordForm medicalRecordWindow = new MedicalRecordForm(medical);
+				//medicalRecordWindow.Show();
+			}
+			else
+			{
+				MessageBox.Show("Please select ONLY ONE row for viewing medical record.", "Error");
+			}
 		}
 
 		private void AmnesisBtnClick(object sender, EventArgs e)
