f8d410f0e42610703c9b4bb9404a057144e26175
SelenaMilutin <selena.milutin@gmail.com>
2022-04-29 03:23:10 +0200

    Add action for save button in AddAppointment

--- view/DoctorView/AddAppointmentForm.cs
+++ view/DoctorView/AddAppointmentForm.cs
@@ -43,7 +43,63 @@ namespace HealthCareInfromationSystem.view.DoctorView
 
 		private void SaveBtnClick(object sender, EventArgs e)
 		{
-			 
+			string premiseId = premiseComboBox.SelectedValue.ToString();
+			//Console.WriteLine(premiseComboBox.SelectedValue.ToString());
+			string patientId = patientComboBox.SelectedValue.ToString();
+			string beginning = beginningTextBox.Text;
+			string duration = durationTextBox.Text;
+			string type = typeComboBox.SelectedItem.ToString();
+			//Console.WriteLine(typeComboBox.SelectedItem.ToString());
+
+			//checking if data is correct
+			try
+			{
+				int durationCheck = int.Parse(duration);
+				DateTime beginningCheck = DateTime.ParseExact(beginning, "dd.MM.yyyy. HH:mm", null);
+				if (type == "physical" && durationCheck != 15) {
+					MessageBox.Show("Physical can only last 15 minutes.", "Error");
+					return;
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Please check beginning or duration field and enter correct values.", "Error");
+				return;
+			}
+
+			//checking if the doctor is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+				"select * from appointments where doctorId =\"" + LoggedInUser.loggedIn.Id + "\"",
+				beginning, duration))
+			{
+				MessageBox.Show("Doctor has an appointment.", "Error");
+				return;
+			}
+
+			//checking if the room is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+			"select * from appointments where premiseId =\"" + premiseId + "\"",
+			beginning, duration))
+			{
+				MessageBox.Show("Premise occupied.", "Error");
+				return;
+			}
+
+			//checking if the patient is available
+			if (!AppointmentController.IsAvailable(Constants.connectionString,
+			"select * from appointments where patientId=\"" + patientId + "\"",
+			beginning, duration))
+			{
+				MessageBox.Show("Patient has an appointment.", "Error");
+				return;
+			}
+
+			DialogResult dialogResult = MessageBox.Show("Are you sure you want to save changes?", "Check", MessageBoxButtons.YesNo);
+			if (dialogResult == DialogResult.Yes)
+			{
+				MessageBox.Show("Changes saved.", "Success");
+				AppointmentController.AddToBase(patientId, premiseId, LoggedInUser.loggedIn.Id, beginning, duration, type);
+			}
 		}
 
 		private void CancelBtnClick(object sender, EventArgs e)
