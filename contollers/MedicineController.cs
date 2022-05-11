c764f7d229c1cade674f30cb7a5b4bb22f28958a
SelenaMilutin <selena.milutin@gmail.com>
2022-05-11 23:00:50 +0200

    Add checking if patient is alergic to a medicine

--- contollers/MedicineController.cs
+++ contollers/MedicineController.cs
@@ -54,5 +54,25 @@ namespace HealthCareInfromationSystem.contollers
                 return medicine;
             }
         }
+
+		internal static Medicine LoadOneById(string medicineId)
+		{
+            using (OleDbConnection connection = new OleDbConnection(Constants.connectionString))
+            {
+
+                OleDbCommand command = new OleDbCommand("select * from medicine where id=\"" + medicineId + "\"", connection);
+
+                connection.Open();
+                OleDbDataReader reader = command.ExecuteReader();
+                Medicine medicine = null;
+
+				while (reader.Read())
+                {
+                    medicine = Medicine.Parse(reader);
+                }
+				reader.Close();
+                return medicine;
+            }
+        }
 	}
 }
--- view/DoctorView/AddPrescriptionForm.cs
+++ view/DoctorView/AddPrescriptionForm.cs
@@ -18,6 +18,7 @@ namespace HealthCareInfromationSystem.view.DoctorView
 	{
 		private int patientId;
 
+
 		public AddPrescriptionForm()
 		{
 			InitializeComponent();
@@ -30,7 +31,6 @@ namespace HealthCareInfromationSystem.view.DoctorView
 			Person patient = PersonController.LoadOnePerson(Constants.connectionString,
 							"select * from users where id=\"" + patientId + "\"");
 			patientFullNameLabel.Text = patient.FirstName + " " + patient.LastName;
-
 			Dictionary<string, string> medicinePair = MedicineController.LoadPair("select id, name from medicine where isVerified = \"true\" ");
 			medicineComboBox.DataSource = new BindingSource(medicinePair, null);
 			medicineComboBox.DisplayMember = "Value";
@@ -45,12 +45,16 @@ namespace HealthCareInfromationSystem.view.DoctorView
 
 		private void SaveClick(object sender, EventArgs e)
 		{
-			string doctorId = periodComboBox.SelectedValue.ToString();
-			//Console.WriteLine(premiseComboBox.SelectedValue.ToString());
-			string specialisation = medicineComboBox.SelectedValue.ToString();
-			if (doctorId == "" && specialisation == "")
+			MedicalRecord medical = MedicalRecordController.LoadMedical(Constants.connectionString,
+							"select * from medical_record where patientId=\"" + patientId + "\""); 
+			string medicineId = medicineComboBox.SelectedValue.ToString();
+			Medicine medicine = MedicineController.LoadOneById(medicineId);
+			string tymeOfConsumption = periodComboBox.SelectedItem.ToString();
+			string quantity = quantityTextBox.Text;
+
+			if (medical.IsAlergic(medicine.Ingredients))
 			{
-				MessageBox.Show("Both fields for doctor and specialisation can not be ampty", "Error");
+				MessageBox.Show("Patient is alergic to an ingredient", "Error");
 				return;
 			}
 			DialogResult dialogResult = MessageBox.Show("Are you sure you want to save changes?", "Check", MessageBoxButtons.YesNo);
