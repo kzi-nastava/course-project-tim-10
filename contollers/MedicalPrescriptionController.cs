332773a34194062a213f3783bd6b179703352848
SelenaMilutin <selena.milutin@gmail.com>
2022-05-11 23:41:45 +0200

    Add medical prescription

---
+++ contollers/MedicalPrescriptionController.cs
@@ -0,0 +1,27 @@
+﻿using HealthCareInfromationSystem.models.entity;
+using HealthCareInfromationSystem.utils;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareInfromationSystem.contollers
+{
+	class MedicalPrescriptionController
+	{
+        public void SaveToBase(MedicalPrescription medicalPrescription)
+        {
+            using (OleDbConnection connection = new OleDbConnection(Constants.connectionString))
+            {
+                connection.Open();
+                String query = $"insert into medical_prescriiption values (\"{medicalPrescription.Id}\", \"{medicalPrescription.Medicine.Id}\", " +
+                                                                         $"\"{medicalPrescription.Quantity}\", \"{medicalPrescription.TimeOfConsumption}\", " +
+                                                                         $"\"{medicalPrescription.Patient.Id}\")";
+                OleDbCommand command = new OleDbCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- models/entity/MedicalPrescription.cs
+++ models/entity/MedicalPrescription.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCareInfromationSystem.models.users;
+
 
 namespace HealthCareInfromationSystem.models.entity
+
 {
 	class MedicalPrescription
 	{
+		int _id;
+		Medicine _medicine;
+		string quantity;
+		Medicine.DrinkingPeriod _timeOfConsumption;
+		Person patient;
+
+		public int Id { get => _id; set => _id = value; }
+		public Medicine Medicine { get => _medicine; set => _medicine = value; }
+		public string Quantity { get => quantity; set => quantity = value; }
+		public Medicine.DrinkingPeriod TimeOfConsumption { get => _timeOfConsumption; set => _timeOfConsumption = value; }
+		public Person Patient { get => patient; set => patient = value; }
 	}
 }
