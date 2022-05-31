4d7d023f297e9ea8290457732c6253e5d3862fe7
unknown <spremic.katarina@gmail.com>
2022-05-31 23:35:02 +0200

    Add Form for supplying Dynamic Equipment out of stock

--- models/entity/Equipment.cs
+++ models/entity/Equipment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthCareInfromationSystem.contollers;
+using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace HealthCareInfromationSystem.models.entity
         private String name;
         private int quantity;
         private int id;
+		private Premise premise;
 
 		public Equipment(string name, int quantity, int id)
 		{
@@ -21,9 +23,18 @@ namespace HealthCareInfromationSystem.models.entity
 			this.Id = id;
 		}
 
+		public Equipment(string name, int quantity, int id, Premise premise)
+        {
+			this.name = name;
+			this.quantity = quantity;
+			this.id = id;
+			this.premise = premise;
+		}
+
 		public string Name { get => name; set => name = value; }
         public int Quantity { get => quantity; set => quantity = value; }
 		public int Id { get => id; set => id = value; }
+		public Premise Premise { get => premise; set => premise = value; }
 
 		internal static Equipment Parse(OleDbDataReader reader)
 		{
@@ -32,5 +43,14 @@ namespace HealthCareInfromationSystem.models.entity
             string name = reader[0].ToString();
             return new Equipment(name, quantity, id);
         }
+
+		internal static Equipment ParseWithPremise(OleDbDataReader reader)
+        {
+			string name = reader[0].ToString();
+			int quantity = int.Parse(reader[1].ToString());
+			int id = int.Parse(reader[2].ToString());
+			Premise premise = PremiseController.GetPremiseById(reader[3].ToString());
+			return new Equipment(name, quantity, id, premise);
+		}
 	}
 }
