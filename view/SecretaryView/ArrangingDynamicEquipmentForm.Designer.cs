bd72106eb8f0dafd9415c50900d0dcf7c03e07b2
unknown <spremic.katarina@gmail.com>
2022-06-01 15:31:39 +0200

    Add Form for arranging equipment

---
+++ view/SecretaryView/ArrangingDynamicEquipmentForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace HealthCareInfromationSystem.view.SecretaryView
+{
+    partial class ArrangingDynamicEquipmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbEquipmentName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbToPremise = new System.Windows.Forms.TextBox();
+            this.tbFromPremise = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(856, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cbEquipmentName
+            // 
+            this.cbEquipmentName.FormattingEnabled = true;
+            this.cbEquipmentName.Location = new System.Drawing.Point(227, 54);
+            this.cbEquipmentName.Name = "cbEquipmentName";
+            this.cbEquipmentName.Size = new System.Drawing.Size(261, 28);
+            this.cbEquipmentName.TabIndex = 1;
+            this.cbEquipmentName.SelectedValueChanged += new System.EventHandler(this.CbEquipmentName_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select equipment:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(331, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Premises low on stock for selected equipment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 562);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(379, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Premises with sufficent stock for selected equipment";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(293, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Selected premise to move equipment to:";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(47, 608);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(867, 184);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 817);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(295, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Selected premise to take equiment from:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 873);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(187, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Input quantity for moving:";
+            // 
+            // tbToPremise
+            // 
+            this.tbToPremise.Location = new System.Drawing.Point(435, 454);
+            this.tbToPremise.Name = "tbToPremise";
+            this.tbToPremise.ReadOnly = true;
+            this.tbToPremise.Size = new System.Drawing.Size(168, 26);
+            this.tbToPremise.TabIndex = 12;
+            // 
+            // tbFromPremise
+            // 
+            this.tbFromPremise.Location = new System.Drawing.Point(435, 817);
+            this.tbFromPremise.Name = "tbFromPremise";
+            this.tbFromPremise.ReadOnly = true;
+            this.tbFromPremise.Size = new System.Drawing.Size(168, 26);
+            this.tbFromPremise.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(435, 870);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 26);
+            this.textBox1.TabIndex = 14;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(383, 962);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(195, 55);
+            this.btnConfirm.TabIndex = 15;
+            this.btnConfirm.Text = "Confirm Move";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.BtnConfirm_Click);
+            // 
+            // ArrangingDynamicEquipmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 1050);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbFromPremise);
+            this.Controls.Add(this.tbToPremise);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbEquipmentName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ArrangingDynamicEquipmentForm";
+            this.Text = "Arranging Dynamic Equipment";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbEquipmentName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbToPremise;
+        private System.Windows.Forms.TextBox tbFromPremise;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
---
+++ view/SecretaryView/ArrangingDynamicEquipmentForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthCareInfromationSystem.view.SecretaryView
+{
+    public partial class ArrangingDynamicEquipmentForm : Form
+    {
+        public ArrangingDynamicEquipmentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CbEquipmentName_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
