2912467af3b91afc275f6cdd646bf377e9e1589f
unknown <spremic.katarina@gmail.com>
2022-06-14 22:30:23 +0200

    Add form for vacation requests

---
+++ view/SecretaryView/VacationRequestForm.cs
@@ -0,0 +1,90 @@
+﻿using HealthCareInfromationSystem.Core.Appointment.VacationRequest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthCareInfromationSystem.view.SecretaryView
+{
+    public partial class VacationRequestForm : Form
+    {
+        private VacationRequestController requestController = new VacationRequestController();
+        private string selectedRequestId;
+        public VacationRequestForm()
+        {
+            InitializeComponent();
+            InitializeActionComboBox();
+            DisplayRequestsTable();
+        }
+
+        private void InitializeActionComboBox()
+        {
+            lblReason.Hide();
+            cbAction.Items.Add("Accept");
+            cbAction.Items.Add("Decline");
+        }
+
+        private void DisplayRequestsTable()
+        {
+            dataGridViewRequests.Rows.Clear();
+            foreach (List<string> row in requestController.GetRowsForRequests())
+            {
+                dataGridViewRequests.Rows.Add(row[0], row[1], row[2], row[3], row[4], row[5]);
+            }
+        }
+
+        private void DataGridViewRequests_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (dataGridViewRequests.Rows[e.RowIndex].Cells[0].Value != null)
+            {
+                selectedRequestId = dataGridViewRequests.Rows[e.RowIndex].Cells[0].Value.ToString();
+            }
+            else
+            {
+                selectedRequestId = "";
+            }
+        }
+
+        private void BtnConfirm_Click(object sender, EventArgs e)
+        {
+            if (CheckIfRequestSelected() && CheckReasonInput())
+            {
+                if (cbAction.SelectedItem.ToString() == "Accept") requestController.Accept(selectedRequestId);
+                else requestController.Decline(selectedRequestId, tbDeclineReason.ToString());
+            }
+        }
+
+        private bool CheckIfRequestSelected()
+        {
+            if (selectedRequestId != "") return true;
+            else MessageBox.Show("No vacation request selected. Please select one.");
+            return false;
+        }
+
+        private bool CheckReasonInput()
+        {
+            if (tbDeclineReason.Text != "" || cbAction.SelectedItem.ToString() == "Accept") return true;
+            else MessageBox.Show("Input for decline reason is unfilled. Please provide a reason.");
+            return false;
+        }
+
+        private void CbAction_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (cbAction.SelectedItem.ToString() == "Decline")
+            {
+                lblReason.Show();
+                tbDeclineReason.Enabled = true;
+            }
+            else
+            {
+                lblReason.Hide();
+                tbDeclineReason.Enabled = false;
+            }
+        }
+    }
+}
